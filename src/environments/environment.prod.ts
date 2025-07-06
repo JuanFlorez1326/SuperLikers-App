3cccb030243cb93ec4cdf590f459530d9d646041
Juan Forez <juanks339@gmail.com>
2025-07-06 10:08:34 -0500

    fix: design

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiKey: '32e608447ff50d5b6760c335ffe87262',
-  baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.superlikerslabs.com/v1'
+  // baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.superlikerslabs.com/v1',
+  baseUrl: 'https://server-superlikers.onrender.com/https://api.superlikerslabs.com/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   apiKey: '32e608447ff50d5b6760c335ffe87262',
-  baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.superlikerslabs.com/v1'
+  // baseUrl: 'https://cors-anywhere.herokuapp.com/https://api.superlikerslabs.com/v1',
+  baseUrl: 'https://server-superlikers.onrender.com/https://api.superlikerslabs.com/v1'
 };
 
 /*
